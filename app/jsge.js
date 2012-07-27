05b27d41d4be7f05a23745cb2f2d5a130a9557f2
Boris Kozorovitzky <a>
2012-07-27 09:54:04 +0200

    Initial Sprite code.

--- app/jsge.js
+++ app/jsge.js
@@ -2,19 +2,35 @@ var JSGE = function() {
     
     var _screen;
     var _sprites = [];
+    var checkScreen = function(){
+            if (_screen === undefined){
+                throw new Error("The screen is undefined, call setScreen with the screen id to initialize it");
+            }
+        
+    };
     
     var self = {
         version : "0.1",
+        
+        setScreen:function(screenId){
+            _screen = $("#"+screenId);
+            if (_screen === undefined){
+                throw new Error("Failed to set screen with id " + screenId);
+            }
+            _screen.css("z-index", 0);
+            _screen.css("position","relative");
+        },
 
         //Starts a game loop and uses the object with screenId as the screen
-        start:function(screenId){
-            _screen = $("#"+screenId);
+        start:function(){
+            checkScreen();
             self.gameLoop.start(function(){
                self.tick(); 
             })
         },
         
         addSprite: function(){
+            checkScreen();
             _sprites.push(new JSGE.Sprite(_screen));
         },
         
--- app/sprite.js
+++ app/sprite.js
@@ -1,5 +1,11 @@
 JSGE.Sprite = function(mainScreen){
   var div = $("<div></div>").css("background-color","yellow");
+  div.css("z-index",100);
+  div.css("width",10);
+  div.css("height",10);
+  div.css("position","absolute");
+  div.css("top",0);
+  div.css("left",0);
   mainScreen.append(div);
 }
 
